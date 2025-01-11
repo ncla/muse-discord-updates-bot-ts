1d46968bab66f72292c26633eb6c9c4d2e161dba
ncla <hi@ncla.me>
2025-01-11 20:04:46 +0200

    Use fake timers in another request manager test

--- tests/request-manager.test.ts
+++ tests/request-manager.test.ts
@@ -49,24 +49,35 @@ test('it sends multiple requests without hitting burst limit', async () => {
         })
     }
 
-    const result = await manager.sendAll(REQUEST_AMOUNT_PER_DURATION, RATE_LIMIT_DURATION)
+    vi.useFakeTimers()
+    vi.advanceTimersByTimeAsync(2000)
 
-    const timestamps = result
-        .filter((response) => {
-            return response.status === 'fulfilled'
+    return manager
+        .sendAll(REQUEST_AMOUNT_PER_DURATION, RATE_LIMIT_DURATION)
+        .then((result) => {
+            const timestamps = result
+                .filter((response) => {
+                    return response.status === 'fulfilled'
+                })
+                .map((response) => {
+                    return Number(response.value.statusText)
+                })
+                .sort()
+
+            expect(Array.isArray((result))).toBe(true)
+            expect(result.length).toBe(REQUEST_AMOUNT)
+            expect(managerSpy).toHaveBeenCalledTimes(REQUEST_AMOUNT)
+
+            // This is a bit primitive, but we are testing if time between requests is at least a second
+            expect(timestamps[5] - timestamps[0]).toBeGreaterThanOrEqual(RATE_LIMIT_DURATION * 1000)
+            expect(timestamps[10] - timestamps[5]).toBeGreaterThanOrEqual(RATE_LIMIT_DURATION * 1000)
         })
-        .map((response) => {
-            return Number(response.value.statusText)
+        .catch((error) => {
+            console.error(error)
         })
-        .sort()
-
-    expect(Array.isArray((result))).toBe(true)
-    expect(result.length).toBe(REQUEST_AMOUNT)
-    expect(managerSpy).toHaveBeenCalledTimes(REQUEST_AMOUNT)
-
-    // This is a bit primitive, but we are testing if time between requests is at least a second
-    expect(timestamps[5] - timestamps[0]).toBeGreaterThanOrEqual(RATE_LIMIT_DURATION * 1000)
-    expect(timestamps[10] - timestamps[5]).toBeGreaterThanOrEqual(RATE_LIMIT_DURATION * 1000)
+        .finally(() => {
+            vi.useRealTimers()
+        })
 })
 
 test('request is tried three times', async () => {
