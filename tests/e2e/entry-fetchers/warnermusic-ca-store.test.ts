2cf8d1eb1fa07e4d9977900749dd7177c2e85245
ncla <hi@ncla.me>
2025-03-01 13:44:19 +0200

    First E2E test, testing Warner Music CA store fetcher

---
+++ tests/e2e/entry-fetchers/warnermusic-ca-store.test.ts
@@ -0,0 +1,12 @@
+import {expect, test, vi, beforeEach } from 'vitest'
+import {WarnerMusicCanadaStore} from "@/src/entry-fetchers/warnermusic-ca-store";
+
+test('it fetches entries', async () => {
+    const fetcher = new WarnerMusicCanadaStore()
+    const result = await fetcher.fetch()
+
+    expect(result.length).toBeGreaterThan(0)
+    expect(typeof result[0].title).toBe('string')
+    expect(typeof result[0].url).toBe('string')
+    expect(typeof result[0].image_url).toBe('string')
+})
